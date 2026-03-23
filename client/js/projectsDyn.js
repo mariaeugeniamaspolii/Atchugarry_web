b91e49353172a5af6d7ee168c481042937010224
Maria Eugenia <maria.maspoli@outlook.es>
2026-03-23 12:13:08 -0300

    image hover routes fix

--- client/js/projectsDyn.js
+++ client/js/projectsDyn.js
@@ -73,7 +73,7 @@ document.addEventListener("DOMContentLoaded", () => {
                             <div class="image-container ${project.imageCover.format}">
                                 <img class="card-image base-image" src="../${project.imageCover.src}" alt="${project.alt}">
                                 <div class="hover-image bg-background">
-                                    <img src="${project.imageHover}" class="hover-image hover-white" alt="Hover Image">
+                                    <img src="../${project.imageHover}" class="hover-image hover-white" alt="Hover Image">
                                 </div>
                             </div>
                             <div class="card-body">
